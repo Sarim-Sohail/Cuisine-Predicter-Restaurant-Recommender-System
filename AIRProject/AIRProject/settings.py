e37f6e1d16dfe8a98e1ece06cf99fd1507d8e18e
Sarim-Sohail <sarimsohail@gmail.com>
2022-06-24 22:56:14 +0500

    Update settings.py

--- AIRProject/AIRProject/settings.py
+++ AIRProject/AIRProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l1vz4df+q0%=a15_u5xkxor-s3%mn(25t^lf51cd(icm&@2w=a'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
